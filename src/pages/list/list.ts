82c08fb0b171672aa7a095bd2f69a1088a93a4d9
LAPTOP-ETV0TKBI\joshu <joshua.nicholl12@outlook.com>
2017-06-21 18:44:46 +1000

    Added create room function and pushed the rooms to an array.

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  items: FirebaseObjectObservable<any>;
+  rooms: Array<{name:string}>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public afAuth: AngularFireAuth, public db: AngularFireDatabase) {
+  this.items = db.object('/rooms', { preserveSnapshot: true });
+	  this.items.subscribe(snapshot => {
+      if(snapshot.val() == null){
+        console.log("No rooms here");
+      }
+      for(var i = 0; i < snapshot.val().length; i++){
+	       this.rooms.push(snapshot.val()[i]);
+      }
+	  });
   }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
+  createRoom(name){
+    this.items.subscribe(snapshot => {
+      this.db.database.ref('rooms').child(name).set({id:snapshot.val()+1});
     });
   }
 }
